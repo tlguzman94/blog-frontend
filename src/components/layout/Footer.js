a09e28c3f63b2d060a1313360663cc148f8ca2ab
Tony G <tlguzman94@gmail.com>
2020-09-16 15:27:36 -0400

    updated footer links to open in new tab

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,10 +9,34 @@ function Footer() {
       className="justify-content-between mt-auto"
     >
       <Nav>
-        <Nav.Link href="#">Facebook</Nav.Link>
-        <Nav.Link href="#">Twitter</Nav.Link>
-        <Nav.Link href="#">Youtube</Nav.Link>
-        <Nav.Link href="#">Github</Nav.Link>
+        <Nav.Link
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </Nav.Link>
+        <Nav.Link
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </Nav.Link>
+        <Nav.Link
+          href="https://www.Youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Youtube
+        </Nav.Link>
+        <Nav.Link
+          href="https://www.Github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </Nav.Link>
       </Nav>
       <Navbar.Text>Created using React</Navbar.Text>
     </Navbar>
