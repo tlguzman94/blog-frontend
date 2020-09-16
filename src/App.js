4e4a4ef53edcaef57a33508d21adaad31190639a
Tony G <tlguzman94@gmail.com>
2020-09-16 14:12:22 -0400

    added react router to manage site routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navigation from './components/layout/Navigation';
 import Footer from './components/layout/Footer';
+import PostList from './components/blog/PostList';
 
 function App() {
   return (
-    <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-      <Navigation />
-      <Footer />
-    </div>
+    <Router>
+      <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
+        <Navigation />
+
+        <Switch>
+          <Route path="/">Homepage</Route>
+          <Route path="/posts">
+            <PostList />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
