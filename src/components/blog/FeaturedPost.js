314b3e989e7ca09f3328a0c610eb7a0160315e5d
Tony G <tlguzman94@gmail.com>
2020-09-17 15:41:18 -0400

    modified featured post component to not show anything until post is loaded

--- src/components/blog/FeaturedPost.js
+++ src/components/blog/FeaturedPost.js
@@ -3,19 +3,12 @@ import { Container } from 'react-bootstrap';
 import Post from './Post';
 
 function FeaturedPost({ post }) {
-  if (!post) {
-    return (
-      <Container>
-        <p className="text-primary lead text-center text-underline">No posts</p>
-      </Container>
-    );
-  }
   return (
     <Container fluid="md" className="my-5">
       <p className="text-primary lead text-center text-underline">
         Featured Post
       </p>
-      <Post post={post} />
+      {post && <Post post={post} />}
     </Container>
   );
 }
