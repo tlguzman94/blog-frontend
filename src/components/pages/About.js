f3ecb4728bb97e15aca8d1f4a5ccbd655efdbaf4
Tony G <tlguzman94@gmail.com>
2020-09-17 15:03:10 -0400

    modified the about page text with lorem ipsum

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,32 +7,41 @@ function About() {
       <p className="text-primary lead text-center text-underline">About</p>
       <Row className="justify-content-center">
         <Col md={12} lg={8}>
-          <h2 className="font-weight-bold text-center">About Title</h2>
+          <h2 className="font-weight-bold text-center">Lorem Ipsum</h2>
           <p>
-            Camden Lock is a small part of Camden Town, London Borough of
-            Camden, England, which was formerly a wharf with stables on the
-            Regent’s Canal. It is immediate to the north of Hampstead Road
-            Locks, a twin operated the lock works. Camden Lock is a small part
-            of Camden Town, London Borough of Camden, England, which was
-            formerly a wharf with stables on the Regent’s Canal. It is immediate
-            to the north of Hampstead Road Locks, a twin operated the lock
-            works. Camden Lock is a small part of Camden Town, London Borough of
-            Camden, England, which was formerly a wharf with stables on the
-            Regent’s Canal. It is immediate to the north of Hampstead Road
-            Locks, a twin operated the lock works. Camden Lock is a small part.
+            <strong>
+              Facere possimus, omnis voluptas assumenda est, omnis dolor
+              repellendus.
+            </strong>
+            <em>
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque
+              nihil impedit quo minus id quod maxime placeat.
+            </em>
+            Esse cillum dolore eu fugiat nulla pariatur.
           </p>
           <p>
-            Camden Lock is a small part of Camden Town, London Borough of
-            Camden, England, which was formerly a wharf with stables on the
-            Regent’s Canal. It is immediate to the north of Hampstead Road
-            Locks, a twin operated the lock works. Camden Lock is a small part
-            of Camden Town, London Borough of Camden, England, which was
-            formerly a wharf with stables on the Regent’s Canal. It is immediate
-            to the north of Hampstead Road Locks, a twin operated the lock
-            works. Camden Lock is a small part of Camden Town, London Borough of
-            Camden, England, which was formerly a wharf with stables on the
-            Regent’s Canal. It is immediate to the north of Hampstead Road
-            Locks, a twin operated the lock works. Camden Lock is a small part.
+            Ut aut reiciendis voluptatibus maiores alias consequatur aut
+            perferendis doloribus asperiores repellat. Fugiat quo voluptas nulla
+            pariatur? Nam libero tempore, cum soluta nobis est eligendi optio
+            cumque nihil impedit quo minus id quod maxime placeat.
+          </p>
+          <ol>
+            <li>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa.
+            </li>
+            <li>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa.
+            </li>
+            <li>
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque
+              nihil impedit quo minus id quod maxime placeat.
+            </li>
+          </ol>
+          <p>
+            Quia consequuntur magni dolores eos qui ratione voluptatem sequi
+            nesciunt. Esse cillum dolore eu fugiat nulla pariatur. Animi, id est
+            laborum et dolorum fuga. Ut enim ad minima veniam, quis nostrum
+            exercitationem ullam corporis suscipit laboriosam.
           </p>
         </Col>
       </Row>
